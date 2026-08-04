03834458474eba064c0cd6576939fadf68cfccfb
amiel matilla <amielmatilla300@gmail.com>
2026-08-04 11:21:55 +0800

    Bumped Toast's Elevation and added zIndex

--- components/Toast.jsx
+++ components/Toast.jsx
@@ -59,7 +59,8 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 4 },
     shadowOpacity: 0.2,
     shadowRadius: 8,
-    elevation: 6,
+    elevation: 999,
+    zIndex: 999,
     maxWidth: '85%',
   },
   iconWrapper: {
