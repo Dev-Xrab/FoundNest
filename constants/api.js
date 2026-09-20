38995b10bd5138c5652cf269bc3068a693fa6494
amiel matilla <amielmatilla300@gmail.com>
2026-09-20 13:52:38 +0800

    added ADMIN_WEB_URL constant

--- constants/api.js
+++ constants/api.js
@@ -1,2 +1,3 @@
 export const API_BASE_URL = "https://foundnest-backend.onrender.com";
 export const DESCRIBE_ITEM_PATH = "/api/gemini-item-listing/describe-item";
+export const ADMIN_WEB_URL = "https://foundnest.vercel.app/login";
